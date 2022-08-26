59a72b2ac49de27452822b77dcd0c3c3ff54e0ea
caketi <zjgzlp@gmail.com>
2022-08-26 18:49:34 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl;
+	cout << __cplusplus << endl; // 199711
+	cout << __cplusplus << endl; // 199711  / 202004
+	return 0;
+}
